82967480839d075ac6d1eb565e7a36e0f2f026d3
Alexander Loktionov <loktionov129@gmail.com>
2018-07-01 13:43:11 +0700

    build: update shell scripts

--- scripts/bash/ci/5_deploy.sh
+++ scripts/bash/ci/5_deploy.sh
@@ -1,4 +1,9 @@
 COMMANDS_TO_RUN+=('echo 5_deploy.sh ...')
+
 COMMANDS_TO_RUN+=('npx semantic-release --branch $BRANCH')
+
+COMMANDS_TO_RUN+=('git remote add github https://github.com/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME.git')
+COMMANDS_TO_RUN+=('git pull github $BRANCH')
+
 COMMANDS_TO_RUN+=('bash scripts/bash/bash_runner.sh scripts/bash/gitlab_sync.sh')
 COMMANDS_TO_RUN+=('bash scripts/bash/bash_runner.sh scripts/bash/docker/deploy.sh')
--- scripts/bash/docker/compose.sh
+++ scripts/bash/docker/compose.sh
@@ -5,7 +5,7 @@ set -u
 
 ENV=${1:-prod}
 
- (cd ./docker/ && docker-compose -f docker-compose.$ENV.yml up -d)
+(cd ./docker/ && docker-compose -f docker-compose.$ENV.yml up -d)
 
 #  TODO: create docker-compose.yml and push to them all basic configurations
 #  (cd ./docker/ && docker-compose -f docker-compose.yml -f docker-compose.$ENV.yml up -d)
