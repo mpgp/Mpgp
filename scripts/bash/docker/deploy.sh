7fa3cd24ef8e3824ada7b01e1865dc62c860e034
Alexander Loktionov <loktionov129@gmail.com>
2018-07-12 13:26:55 +0700

    ci: build mpgpdb docker image and deploy it to hub

--- scripts/bash/docker/deploy.sh
+++ scripts/bash/docker/deploy.sh
@@ -1,4 +1,8 @@
 COMMANDS_TO_RUN+=('echo Build image and push to hub.docker.com')
 COMMANDS_TO_RUN+=('echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin')
-COMMANDS_TO_RUN+=('bash scripts/bash/docker/build.sh')
-COMMANDS_TO_RUN+=('docker push $DOCKER_IMAGE_OWNER/$DOCKER_IMAGE_NAME')
+
+COMMANDS_TO_RUN+=('bash scripts/bash/docker/build.sh $DOCKER_IMAGE_NAME_WEB')
+COMMANDS_TO_RUN+=('docker push $DOCKER_IMAGE_OWNER/$DOCKER_IMAGE_NAME_WEB')
+
+COMMANDS_TO_RUN+=('bash scripts/bash/docker/build.sh $DOCKER_IMAGE_NAME_DB')
+COMMANDS_TO_RUN+=('docker push $DOCKER_IMAGE_OWNER/$DOCKER_IMAGE_NAME_DB')
